fad625a38cc04e6f7732d5cf8c5e70da56d442d8
kennyjung0223 <kenrohs@gmail.com>
2021-01-19 21:31:39 -0800

    order pay reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,7 +14,8 @@ import {
 } from './reducers/userReducers.js'
 import {
   orderCreateReducer,
-  orderDetailseReducer,
+  orderDetailsReducer,
+  orderPayReducer,
 } from './reducers/orderReducers'
 
 const reducer = combineReducers({
@@ -26,7 +27,8 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
-  orderDetails: orderDetailseReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
